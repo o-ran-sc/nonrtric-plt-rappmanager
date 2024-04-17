aa60df102eb3ef47d173b4259b5127b41ce2bce1
aravind.est <aravindhan.a@est.tech>
2024-04-17 17:26:45 +0100

    Enable servicemanager installation
    Service Manager and Kong enabled for installation.
    
    Issue-ID: NONRTRIC-981
    Change-Id: If10799f0b14a5e642eb5a91b85e62d4dcc76cd07
    Signed-off-by: aravind.est <aravindhan.a@est.tech>

--- scripts/install/install-nonrtric.sh
+++ scripts/install/install-nonrtric.sh
@@ -22,8 +22,8 @@ echo "######### Installing NONRTRIC components #########"
 
 git clone "https://gerrit.o-ran-sc.org/r/it/dep"
 
-ENABLED_SERVICES=(installPms installA1controller installA1simulator installInformationservice installrAppmanager installDmeParticipant)
-DISABLED_SERVICES=(installControlpanel installRappcatalogueservice installRappcatalogueenhancedservice installNonrtricgateway installKong installDmaapadapterservice installDmaapmediatorservice installHelmmanager installOrufhrecovery installRansliceassurance installRanpm)
+ENABLED_SERVICES=(installPms installA1controller installA1simulator installInformationservice installrAppmanager installDmeParticipant installCapifcore installServicemanager installKong)
+DISABLED_SERVICES=(installControlpanel installRappcatalogueservice installRappcatalogueenhancedservice installNonrtricgateway installDmaapadapterservice installDmaapmediatorservice installHelmmanager installOrufhrecovery installRansliceassurance installRanpm)
 
 RECEIPE_FILE="dep/nonrtric/RECIPE_EXAMPLE/example_recipe.yaml"
 
