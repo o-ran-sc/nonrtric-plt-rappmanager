36393afecf62cf530208297c64a3ea17fc6d5b7c
sunil.n <sunil.n@samsung.com>
2025-11-10 18:17:55 +0530

    Implement 3GPP 28.532 Modify NetworkSliceSubnet API. It's implemented for demo purpose only. Function logs request and sends OK response
    Change-Id: I5d5d003f8bba938639cd7e48f8dcee4e5528496b
    Signed-off-by: sunil.n <sunil.n@samsung.com>

---
+++ sample-rapp-generator/rapp-ran-nssmf-simulator/ran-nssmf-simulator/src/main/java/org/oransc/ran/nssmf/simulator/dto/SliceProfileDTO.java
@@ -0,0 +1,9 @@
+package org.oransc.ran.nssmf.simulator.dto;
+
+import lombok.Data;
+
+@Data
+public class SliceProfileDTO {
+    // Fields based on SliceProfile from TS28541_SliceNrm.yaml
+    // This is a placeholder for the response body of the modify operation.
+}
